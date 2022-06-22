c0e78f4edfc4b5fd5d2960f9620956a6e10dd451
Carlos Henrique <carloshenriquepvh@hotmail.com>
2022-06-21 20:19:57 -0400

    Terminando a tela de cadastro

---
+++ teste.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<style>
+    div {
+        background: green;
+        height: 100px;
+        width: 100px;
+    }
+
+    .div {
+        background: red;
+        height: 100px;
+        width: 100px;
+    }
+
+    #div {
+        background: blue;
+        height: 100px;
+        width: 100px;
+    }
+
+</style>
+
+<div >Taymerson</div>
+<br>
+<div class="div">Prisco</div>
+<br>
+<div id="div" >Henrique</div>
+
+
+
+
+</body>
+</html>
